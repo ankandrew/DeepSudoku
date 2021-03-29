899bde9108974fafa610747069a920d26b5c6f75
ankandrew <61120139+ankandrew@users.noreply.github.com>
2021-03-29 14:36:14 -0300

    col & row feature extraction

--- deep_sudoku/model/model.py
+++ deep_sudoku/model/model.py
@@ -30,50 +30,45 @@ class SudokuMLP(nn.Module):
         return x
 
 
-# TODO: Add column/row features
 class MultiBranchSudoku(nn.Module):
     def __init__(self, input_channels: int = 10):
         super(MultiBranchSudoku, self).__init__()
         # Local branch (applied at each 3x3 local grid)
         self.nb_conv1 = Conv2dBlock(input_channels, 128, k=3, s=(3, 3), p=0, batch_norm=False, activation=True)
         self.nb_conv2 = Conv2dBlock(128, 128, k=1, s=1, p=0, batch_norm=False, activation=True)
-        self.nb_conv3 = Conv2dBlock(128, 128, k=3, s=1, p=0, batch_norm=False, activation=True)  # From (batch, 128, 3, 3) to (batch, 128, 1, 1)
+        self.nb_conv3 = Conv2dBlock(128, 128, k=3, s=1, p=0, batch_norm=False,
+                                    activation=True)  # From (batch, 128, 3, 3) to (batch, 128, 1, 1)
         # Global branch (applied to the 9x9 global grid)
         self.gb_conv1 = Conv2dBlock(input_channels, 128, k=(9, 9), s=1, p=0, batch_norm=False, activation=True)
-        self.gb_conv2 = Conv2dBlock(128, 128, k=1, s=1, p=0, batch_norm=False, activation=True)  # Conceptully is equal to a FC
+        self.gb_conv2 = Conv2dBlock(128, 128, k=1, s=1, p=0, batch_norm=False,
+                                    activation=True)  # Conceptully is equal to a FC
         # # Row branch (applied to each row)
-        # self.rb_conv1 = Conv1dBlock(input_channels, 128, k=9, s=1, p=0, batch_norm=False, activation=True)
-        # self.rb_conv2 = Conv1dBlock(input_channels, 128, k=9, s=1, p=0, batch_norm=False, activation=True)
+        self.rb_conv1 = Conv1dBlock(input_channels, 128, k=9, s=9, p=0, batch_norm=False, activation=True)
         # # Column branch (applied to each column)
+        self.cb_conv1 = Conv1dBlock(input_channels, 128, k=9, s=9, p=0, batch_norm=False, activation=True)
         # Merge branch
-        self.mb_conv1 = Conv2dBlock(128*2, 64, k=1, s=1, p=0, batch_norm=False, activation=True)
-        self.mb_conv2 = Conv2dBlock(64, 128, k=1, s=1, p=0, batch_norm=False, activation=True)
-        self.mb_conv3 = Conv2dBlock(128, 9*9*9, k=1, s=1, p=0, batch_norm=False, activation=False)
+        self.fc = FullyConnected([128 * 2 + 128*9*2, 512, 9 * 9 * 9], dropout_rate=0.5)
 
     def forward(self, x):
         # Local branch
         x1 = self.nb_conv1(x)
         x1 = self.nb_conv2(x1)
         x1 = self.nb_conv3(x1)  # out -> (batch, channels, 1, 1)
+        x1 = x1.flatten(1, -1)
         # Global branch
         x2 = self.gb_conv1(x)
         x2 = self.gb_conv2(x2)  # out -> (batch, channels, 1, 1)
+        x2 = x2.flatten(1, -1)
+        # Row branch
+        x3 = x.reshape(-1, x.size(1), x.size(2) ** 2)
+        x3 = self.rb_conv1(x3)  # out shape -> (batch, 128, 9)
+        x3 = x3.flatten(1, -1)
+        # Col branch
+        x4 = x.movedim(-2, -1)
+        x4 = x4.reshape(-1, x.size(1), x.size(2) ** 2)
+        x4 = self.cb_conv1(x4)  # out shape -> (batch, 128, 9)
+        x4 = x4.flatten(1, -1)
         # Merge branches
-        x3 = torch.cat([x1, x2], dim=1)
-        x4 = self.mb_conv1(x3)
-        x4 = self.mb_conv2(x4)
-        x4 = self.mb_conv3(x4)
-        return x4
-
-# TEST
-# from deep_sudoku.data.dataset import SudokuDataset
-# from deep_sudoku.transform import ToTensor
-# from torch.utils.data import DataLoader
-#
-# model = HybridSudoku()
-# # inp = torch.randint(0, 2, (1, 1, 9, 9))
-# dataset = SudokuDataset(n=1, transform=ToTensor(one_hot=True))
-# loader = DataLoader(dataset, batch_size=1, shuffle=True)
-# x, y = next(iter(loader))
-# model(x)
-# print()
+        x5 = torch.cat([x1, x2, x3, x4], dim=1)
+        x6 = self.fc(x5)
+        return x6
