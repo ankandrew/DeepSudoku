1b1a37ceec91216f940686cd13d8469d252ff813
ankandrew <61120139+ankandrew@users.noreply.github.com>
2021-04-13 00:49:32 -0300

    option to use 1_million_sudoku ds

--- deep_sudoku/data/dataset.py
+++ deep_sudoku/data/dataset.py
@@ -1,16 +1,37 @@
+import pandas as pd
 from torch.utils.data import Dataset
+from pathlib import Path
+
+from .downloader import download_url
 from .generator import Generator
 
 
 class SudokuDataset(Dataset):
-    def __init__(self, n=500, transform=None):
+    def __init__(self, n=500, transform=None, a: float = 0.6, b: float = 0.8, from_generator: bool = True):
         """
         :param n: number of samples to generate
         :param transform: Optional transform to apply to features
+        :param a: Lower bound probability for keeping values. Must be >= 0
+        :param b: Upper bound probability for keeping values. Must be <= 1
+        :param from_generator: Wether to generate Sudoku games from Generator
+        class or to use the 1 million sudoku dataset from Kaggle
+
+        Example: if a and b are set to 0.6 and 0.8 respectively, the probability
+        of blanking (filling w/ zeros) cells at random will be 1 - 0.6 and 1 - 0.8.
+        This means that in the worst case there will be 40% of missing cells.
+        Note that this is sampled from an uniform distribution.
         """
         super(SudokuDataset, self).__init__()
         self.n = n
-        self.x, self.y = Generator().generate_dataset(n)
+        if from_generator:
+            self.x, self.y = Generator().generate_dataset(n, a, b)
+        else:
+            filename = '1m_sudoku_dataset_parsed.csv'
+            ds_path = Path(__file__).parent / filename
+            # Download if missing
+            download_url(f'https://github.com/ankandrew/DeepSudoku/releases/download/v0.1-alpha/{filename}', ds_path)
+            ds = pd.read_csv(ds_path, header=None, dtype='uint8').to_numpy()
+            self.x, self.y = ds[:, :81].reshape(-1, 9, 9), ds[:, 81:].reshape(-1, 9, 9)
         self.transform = transform
 
     def __len__(self):
