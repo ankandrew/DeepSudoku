17e4f8dc7b649fd1bc33d8ba25c94829b3de1543
ankandrew <61120139+ankandrew@users.noreply.github.com>
2021-05-05 15:52:56 -0300

    rnn wrapper

--- deep_sudoku/model/model.py
+++ deep_sudoku/model/model.py
@@ -3,6 +3,12 @@ from typing import List
 from deep_sudoku.model.block import Conv2dBlock, Conv1dBlock, FullyConnected
 import torch
 
+"""
+TODO:
+* Try Layer Norm in FC (Like transformers)
+* Test ReLU variants in FC (i.e. leaky relu)
+"""
+
 
 class SudokuConv(nn.Module):
     def __init__(self, batch_norm: bool = True):
@@ -47,7 +53,7 @@ class MultiBranchSudoku(nn.Module):
         # # Column branch (applied to each column)
         self.cb_conv1 = Conv1dBlock(input_channels, 128, k=9, s=9, p=0, batch_norm=False, activation=True)
         # Merge branch
-        self.fc = FullyConnected([128 * 2 + 128*9*2, 512, 9 * 9 * 9], dropout_rate=0.5)
+        self.fc = FullyConnected([128 * 2 + 128 * 9 * 2, 512, 9 * 9 * 9], dropout_rate=0.5)
 
     def forward(self, x):
         # Local branch
@@ -72,3 +78,60 @@ class MultiBranchSudoku(nn.Module):
         x5 = torch.cat([x1, x2, x3, x4], dim=1)
         x6 = self.fc(x5)
         return x6
+
+
+class RNN(nn.Module):
+    """
+    Wraps around torch.nn recurrent nets variants. Optional applies a
+    Fully Connected/Dense layer to each hidden state at timestep `t`.
+
+    Examples
+    --------
+    >>> rnn = RNN(10, 128, 1, 'lstm', [256, 9], fc_bn=True, fc_dropout=0.25, bidirectional=True)
+    >>> random_input = torch.randn((64, 81, 10), dtype=torch.float32)
+    >>> out = rnn(random_input)
+    >>> out.shape
+    torch.Size([64, 81, 9])
+
+    Number of classes is assumed from fc_layers last element (fc_layers[-1]).
+
+    Note: Since in this example is bidirectional hidden_state is multiplied by 2,
+    hence fc_layers[-1] must be hidden_state * 2
+    """
+
+    def __init__(self, input_size: int,
+                 hidden_size: int,
+                 n_layers: int,
+                 rnn_type: str = 'lstm',
+                 fc_layers: List[int] = None,
+                 fc_bn: bool = False,
+                 fc_dropout: float = None,
+                 bidirectional: bool = False):
+        super(RNN, self).__init__()
+        if rnn_type == 'rnn':
+            self.rnet = nn.RNN(input_size, hidden_size, n_layers, batch_first=True, bidirectional=bidirectional)
+        elif rnn_type == 'lstm':
+            self.rnet = nn.LSTM(input_size, hidden_size, n_layers, batch_first=True, bidirectional=bidirectional)
+        elif rnn_type == 'gru':
+            self.rnet = nn.GRU(input_size, hidden_size, n_layers, batch_first=True, bidirectional=bidirectional)
+        else:
+            raise ValueError(f'Aviable options are: gru/lstm/rnn but got {rnn_type}')
+        # Cleaner than one-liner
+        if fc_layers is not None:
+            # Calculate hidden state based on bidirectional
+            final_hidden_size = hidden_size * 2 if bidirectional else hidden_size
+            # Check (final_hidden_size output features) == (fc_layers input features)
+            if final_hidden_size != fc_layers[0]:
+                raise ValueError('hidden state dimension must be equal to fc layers first element')
+            self.fc = FullyConnected(fc_layers, batch_norm=fc_bn, dropout_rate=fc_dropout, activation=True)
+        else:
+            self.fc = None
+
+    def forward(self, x):
+        if isinstance(self.rnet, nn.LSTM):
+            out, (h_n, _) = self.rnet(x)  # outs -> (batch, seq_len, hidden_size)
+        else:
+            out, h_n = self.rnet(x)  # outs -> (batch, seq_len, hidden_size)
+        if self.fc is not None:
+            out = self.fc(out)  # outs -> (batch, seq_len, num_classes/fc_layers[-1])
+        return out
