f2792fa5b88ecea1aebcbab6c55ed8ea02f3f74b
ankandrew <61120139+ankandrew@users.noreply.github.com>
2023-05-18 21:43:26 -0300

    Minor changes

--- train.py
+++ train.py
@@ -5,19 +5,10 @@ from stable_baselines3 import PPO
 
 from sudoku_rl.env import SudokuEnv
 
-TOTAL_TIMESTEPS = 2_000_000
+TOTAL_TIMESTEPS = 100_000
 
 env = SudokuEnv()
 # Define and Train the agent
-model = PPO(
-    policy="MlpPolicy",
-    env=env,
-    n_steps=128,
-    batch_size=32,
-    n_epochs=5,
-    gamma=0.999,
-    gae_lambda=0.98,
-    ent_coef=0.01,
-    verbose=1)
+model = PPO("MlpPolicy", env, verbose=2)
 model.learn(total_timesteps=TOTAL_TIMESTEPS)
-model.save(f"trained-models/sudoku-ppo-{TOTAL_TIMESTEPS / 1e6:.2f}m-steps")
+model.save(f"trained-models/sudoku-{model.__class__.__name__}-{TOTAL_TIMESTEPS / 1e6:.2f}m-steps")
