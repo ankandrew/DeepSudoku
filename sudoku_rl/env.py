ecc5b312460b069a128581ae4fbca95772d9d9e5
ankandrew <61120139+ankandrew@users.noreply.github.com>
2023-05-21 12:08:21 -0300

    Make playable cells on non-rigid grid

--- sudoku_rl/env.py
+++ sudoku_rl/env.py
@@ -11,8 +11,8 @@ from gymnasium import spaces
 from sudoku_rl import sudoku_generator, sudoku_validator
 
 WIN_REWARD: float = 1.0
-VALID_ACTION_REWARD: float = 0.0
-INVALID_ACTION_REWARD: float = -0.1
+VALID_ACTION_REWARD: float = 0.01
+INVALID_ACTION_REWARD: float = -0.02
 
 
 class SudokuEnv(gym.Env):
@@ -24,7 +24,13 @@ class SudokuEnv(gym.Env):
         super().__init__()
         self.action_space = spaces.Discrete(9 * 9 * 9)
         self.observation_space = spaces.Box(low=0, high=9, shape=(9 * 9,), dtype=np.int8)
+        self._new_sudoku()
+
+    def _new_sudoku(self) -> None:
         self.solved_grid, self.play_grid = sudoku_generator.generate_9x9_sudoku()
+        self.solved_grid.flags.writeable = False
+        self.rigid_grid = self.play_grid.copy()
+        self.rigid_grid.flags.writeable = False
 
     def _get_info(self):
         return {"filled_cells": np.count_nonzero(self.play_grid)}
@@ -41,7 +47,8 @@ class SudokuEnv(gym.Env):
         col = (action // 9) % 9
         # Determine the number to add (1-9)
         num = action % 9 + 1
-        if self.play_grid[row, col] == 0:
+        # We can play on any cell as long as we don't modify the rigid grid
+        if self.rigid_grid[row, col] == 0:
             # Check if the played action is valid based on Sudoku rules
             play_grid_2 = self.play_grid.copy()
             # Add the number to the grid at the corresponding position
@@ -70,7 +77,7 @@ class SudokuEnv(gym.Env):
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
-        self.solved_grid, self.play_grid = sudoku_generator.generate_9x9_sudoku()
+        self._new_sudoku()
         observation = self._get_obs()
         info = self._get_info()
         return observation, info
