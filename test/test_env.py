1949271dadae50e3a22e956900d6eb2be9f42507
ankandrew <61120139+ankandrew@users.noreply.github.com>
2023-05-21 12:14:49 -0300

    Add fixture to test

--- test/test_env.py
+++ test/test_env.py
@@ -2,26 +2,29 @@
 Test env module
 """
 import numpy as np
+import pytest
 from gymnasium.spaces import Box, Discrete
 from stable_baselines3.common import env_checker
 
 from sudoku_rl.env import INVALID_ACTION_REWARD, SudokuEnv
 
 
-def test_action_space():
-    env = SudokuEnv()
+@pytest.fixture(scope="function")
+def env() -> SudokuEnv:
+    return SudokuEnv()
+
+
+def test_action_space(env: SudokuEnv):
     assert isinstance(env.action_space, Discrete)
     assert env.action_space.n == 9 * 9 * 9
 
 
-def test_observation_space():
-    env = SudokuEnv()
+def test_observation_space(env: SudokuEnv):
     assert isinstance(env.observation_space, Box)
     assert env.observation_space.shape == (9 * 9,)
 
 
-def test_invalid_action():
-    env = SudokuEnv()
+def test_invalid_action(env: SudokuEnv):
     # Pick a cell with a number already in it
     row, col = 0, 0
     while env.play_grid[row, col] == 0:
@@ -34,8 +37,7 @@ def test_invalid_action():
     assert reward == INVALID_ACTION_REWARD
 
 
-def test_reset_changes_grid():
-    env = SudokuEnv()
+def test_reset_changes_grid(env: SudokuEnv):
     old_grid = env.play_grid.copy()
     env.reset()
     new_grid = env.play_grid.copy()
@@ -43,5 +45,5 @@ def test_reset_changes_grid():
         np.testing.assert_array_equal(old_grid, new_grid)
 
 
-def test_env_is_valid() -> None:
-    env_checker.check_env(SudokuEnv())
+def test_env_is_valid(env: SudokuEnv) -> None:
+    env_checker.check_env(env)
