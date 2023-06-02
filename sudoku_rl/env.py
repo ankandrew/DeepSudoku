2810f01f63fb5b32aeb5035a66557b37d1576ed9
ankandrew <61120139+ankandrew@users.noreply.github.com>
2023-06-02 10:41:00 -0300

    Rework action space

--- sudoku_rl/env.py
+++ sudoku_rl/env.py
@@ -2,7 +2,9 @@
 Check: https://www.gymlibrary.dev/content/environment_creation/
 """
 
-from typing import Tuple
+import random
+from dataclasses import astuple, dataclass
+from enum import IntEnum, auto
 
 import gymnasium as gym
 import numpy as np
@@ -14,8 +16,63 @@ WIN_REWARD: float = 1
 """Ultimate reward when agent fills all the Sudoku cells and it's a valid grid."""
 VALID_ACTION_REWARD: float = 0.1
 """Reward given when the agent plays a valid number in a playable cell."""
+NO_REWARD: float = 0.0
+"""No Reward"""
 INVALID_ACTION_REWARD: float = -1
 """Negative reward given to the agent when tries to fill in cells that were originally filled in."""
+
+
+@dataclass
+class Position:
+    """Position"""
+
+    row: int
+    col: int
+
+    def move_left(self) -> None:
+        self.col = max(0, self.col - 1)
+
+    def move_right(self) -> None:
+        self.col = min(8, self.col + 1)
+
+    def move_up(self) -> None:
+        self.row = max(0, self.row - 1)
+
+    def move_down(self) -> None:
+        self.row = min(8, self.row + 1)
+
+    def __post_init__(self) -> None:
+        assert 0 <= self.col <= 8, f"Invalid {self.col} value for col"
+        assert 0 <= self.row <= 8, f"Invalid {self.row} value for row"
+
+
+@dataclass
+class ActionResult:
+    """Action Result"""
+
+    reward: float
+    terminated: bool
+
+    def __iter__(self):
+        return iter(astuple(self))
+
+
+class Action(IntEnum):
+    """Action valid options"""
+
+    MOVE_LEFT = 0
+    MOVE_RIGHT = auto()
+    MOVE_UP = auto()
+    MOVE_DOWN = auto()
+    INSERT_1 = auto()
+    INSERT_2 = auto()
+    INSERT_3 = auto()
+    INSERT_4 = auto()
+    INSERT_5 = auto()
+    INSERT_6 = auto()
+    INSERT_7 = auto()
+    INSERT_8 = auto()
+    INSERT_9 = auto()
 
 
 class SudokuEnv(gym.Env):
@@ -25,12 +82,13 @@ class SudokuEnv(gym.Env):
 
     def __init__(self):
         super().__init__()
-        # In each cell of the 9x9 grid we can put a value from [1, 9], hence 9**3
-        self.action_space = spaces.Discrete(9 * 9 * 9)
+        # We can: go up, down, left, right (4 actions) + place any number from [1, 9] (9 actions)
+        self.action_space = spaces.Discrete(4 + 9)
         # In each cell of the 9x9 grid we can observe a value from [0, 9] where 0 indicates
         # that a value needs to be filled
         self.observation_space = spaces.MultiBinary(9 * 9 * 10)
         self._new_sudoku()
+        self.position = Position(row=random.randint(0, 8), col=random.randint(0, 8))
 
     def _new_sudoku(self) -> None:
         self.solved_grid, self.play_grid = sudoku_generator.generate_9x9_sudoku()
@@ -42,33 +100,60 @@ class SudokuEnv(gym.Env):
     def _get_obs(self):
         return utils.one_hot_9x9_sudoku(self.play_grid).ravel()
 
-    def _play_action(self, action: int) -> Tuple[float, bool]:
-        if not 0 <= action <= 728:
-            raise ValueError(f"Action must range in between [0, 728], got {action}")
-        # Determine the row number (0-8)
-        row = action // 81
-        # Determine the column number (0-8)
-        col = (action // 9) % 9
-        # Determine the number to add (1-9)
-        num = action % 9 + 1
-        if self.play_grid[row, col] == 0:
+    def _play_action(self, action: int) -> ActionResult:
+        if not 0 <= action <= 12:
+            raise ValueError(f"Action must range in between [0, 12], got {action}")
+
+        if action == Action.MOVE_LEFT:
+            self.position.move_left()
+            return ActionResult(reward=NO_REWARD, terminated=False)
+        if action == Action.MOVE_RIGHT:
+            self.position.move_right()
+            return ActionResult(reward=NO_REWARD, terminated=False)
+        if action == Action.MOVE_UP:
+            self.position.move_up()
+            return ActionResult(reward=NO_REWARD, terminated=False)
+        if action == Action.MOVE_DOWN:
+            self.position.move_down()
+            return ActionResult(reward=NO_REWARD, terminated=False)
+
+        if action == Action.INSERT_1:
+            play_number = 1
+        elif action == Action.INSERT_2:
+            play_number = 2
+        elif action == Action.INSERT_3:
+            play_number = 3
+        elif action == Action.INSERT_4:
+            play_number = 4
+        elif action == Action.INSERT_5:
+            play_number = 5
+        elif action == Action.INSERT_6:
+            play_number = 6
+        elif action == Action.INSERT_7:
+            play_number = 7
+        elif action == Action.INSERT_8:
+            play_number = 8
+        elif action == Action.INSERT_9:
+            play_number = 9
+
+        if self.play_grid[self.position.row, self.position.col] == 0:
             # Check if the played action is valid based on Sudoku rules
             play_grid_2 = self.play_grid.copy()
             # Add the number to the grid at the corresponding position
-            play_grid_2[row, col] = num
+            play_grid_2[self.position.row, self.position.col] = play_number
             if sudoku_validator.is_sudoku_valid(play_grid_2):
                 if self.is_episode_done():
-                    return WIN_REWARD, True
+                    return ActionResult(reward=WIN_REWARD, terminated=True)
                 else:
                     # Game continues, persist the new grid
                     self.play_grid = play_grid_2
-                    return VALID_ACTION_REWARD, False
+                    return ActionResult(reward=VALID_ACTION_REWARD, terminated=False)
             else:
                 # We ended up in an invalid Sudoku state
-                return INVALID_ACTION_REWARD, True
+                return ActionResult(reward=INVALID_ACTION_REWARD, terminated=False)
         else:
             # There is already a number in the cell
-            return INVALID_ACTION_REWARD, True
+            return ActionResult(reward=NO_REWARD, terminated=False)
 
     def is_episode_done(self) -> bool:
         # If there are no more 0's the game terminated
--- test/test_env.py
+++ test/test_env.py
@@ -1,12 +1,11 @@
 """
 Test env module
 """
-import numpy as np
 import pytest
 from gymnasium.spaces import Discrete, MultiBinary
 from stable_baselines3.common import env_checker
 
-from sudoku_rl.env import INVALID_ACTION_REWARD, SudokuEnv
+from sudoku_rl.env import SudokuEnv
 
 
 @pytest.fixture(scope="function")
@@ -16,33 +15,12 @@ def env() -> SudokuEnv:
 
 def test_action_space(env: SudokuEnv):
     assert isinstance(env.action_space, Discrete)
-    assert env.action_space.n == 9 * 9 * 9
+    assert env.action_space.n == 4 + 9
 
 
 def test_observation_space(env: SudokuEnv):
     assert isinstance(env.observation_space, MultiBinary)
     assert env.observation_space.shape == (9 * 9 * 10,)
-
-
-def test_invalid_action(env: SudokuEnv):
-    # Pick a cell with a number already in it
-    row, col = 0, 0
-    while env.play_grid[row, col] == 0:
-        col += 1
-        if col > 8:
-            row += 1
-            col = 0
-    action = row * 81 + col * 9 + env.play_grid[row, col]
-    _, reward, *_ = env.step(action)
-    assert reward == INVALID_ACTION_REWARD
-
-
-def test_reset_changes_grid(env: SudokuEnv):
-    old_grid = env.play_grid.copy()
-    env.reset()
-    new_grid = env.play_grid.copy()
-    with np.testing.assert_raises(AssertionError):
-        np.testing.assert_array_equal(old_grid, new_grid)
 
 
 def test_env_is_valid(env: SudokuEnv) -> None:
